3e6c446d278e5b64220220fa138823888f91d839
MarckVinny <mvrpj@msn.com>
2024-06-25 19:43:37 -0300

    style: código formatado

--- src/usecases/criar-registro-financeiro.ts
+++ src/usecases/criar-registro-financeiro.ts
@@ -1,20 +1,37 @@
-import { RegistroFinanceiro, TipoRegistro, StatusRegistro } from '../domain/models/registro-financeiro';
-import { v4 as uuidv4 } from 'uuid';
+import {
+  RegistroFinanceiro,
+  TipoRegistro,
+  StatusRegistro,
+} from '../domain/models/registro-financeiro'
+import { v4 as uuidv4 } from 'uuid'
 
 interface CriarRegistroFinanceiroRequest {
-  descricao: string;
-  data: Date;
-  tipo: TipoRegistro;
-  valor: number;
-  status: StatusRegistro;
+  descricao: string
+  data: Date
+  tipo: TipoRegistro
+  valor: number
+  status: StatusRegistro
 }
 
 export class CriarRegistroFinanceiro {
   executar(request: CriarRegistroFinanceiroRequest): RegistroFinanceiro {
-    const { descricao: descricao, data: date, tipo: type, valor: value, status } = request;
-    const id = uuidv4(); // Gera um ID único para o registro
-    const registroFinanceiro = new RegistroFinanceiro(id, descricao, date, type, value, status);
+    const {
+      descricao: descricao,
+      data: date,
+      tipo: type,
+      valor: value,
+      status,
+    } = request
+    const id = uuidv4() // Gera um ID único para o registro
+    const registroFinanceiro = new RegistroFinanceiro(
+      id,
+      descricao,
+      date,
+      type,
+      value,
+      status
+    )
     // Aqui você pode adicionar lógica adicional, como persistência no banco de dados
-    return registroFinanceiro;
+    return registroFinanceiro
   }
 }
--- src/usecases/criar-usuario.ts
+++ src/usecases/criar-usuario.ts
@@ -1,16 +1,16 @@
-import { Usuario } from '../domain/models/usuario';
+import { Usuario } from '../domain/models/usuario'
 
 interface CriarUsuarioRequest {
-  id: string;
-  nome: string;
-  email: string;
+  id: string
+  nome: string
+  email: string
 }
 
 export class CriarUsuario {
   executar(request: CriarUsuarioRequest): Usuario {
-    const { id, nome: nome, email } = request;
-    const usuario = new Usuario(id, nome, email);
+    const { id, nome: nome, email } = request
+    const usuario = new Usuario(id, nome, email)
     // Aqui você pode adicionar lógica adicional, como persistência no banco de dados
-    return usuario;
+    return usuario
   }
 }
